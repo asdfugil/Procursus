fe0195cc358a37625f77248a3c3e30017233ec97
asdfugil <42699250+asdfugil@users.noreply.github.com>
2021-05-19 13:19:31 +0800

    e

--- xcb-proto.mk
+++ xcb-proto.mk
@@ -20,7 +20,7 @@ xcb-proto: xcb-proto-setup
 		$(DEFAULT_CONFIGURE_FLAGS) \
 		--disable-static \
 		am_cv_python_pythondir=$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/python3/dist-packages \
-		PYTHON=$(shell which python3)
+		PYTHON=/usr/local/bin/python3
 	+$(MAKE) -C $(BUILD_WORK)/xcb-proto install \
 		DESTDIR=$(BUILD_STAGE)/xcb-proto \
 		PYTHON=$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin/python3
