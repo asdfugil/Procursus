4321a6cd8f4680ba78ba0ca802ad47799572dcb2
asdfugil <42699250+asdfugil@users.noreply.github.com>
2021-05-22 20:37:34 +0800

    vlc: disable sftp

--- vlc.mk
+++ vlc.mk
@@ -42,7 +42,7 @@ ifneq ($(wildcard $(BUILD_WORK)/vlc/.build_complete),)
 vlc:
 	@echo "Using previously built vlc."
 else
-vlc: aom dav1d ffmpeg fontconfig freetype frei0r gnutls lame libarchive libass libdvdcss libdvdnav libdvdread libpng16 libsoxr libssh libvidstab libvorbis libvpx libopencore-amr openjpeg libopus libx11 libxft libxcb lua5.4 rav1e rtmpdump rubberband sdl2 libsnappy libspeex libsrt tesseract libtheora libwebp x264 x265 libxvidcore xz  $(VLC_EXTRA_DEPS) vlc-setup
+vlc: aom dav1d ffmpeg fontconfig freetype frei0r gnutls lame libarchive libass libdvdcss libdvdnav libdvdread libpng16 libsoxr libssh2 libvidstab libvorbis libvpx libopencore-amr openjpeg libopus libx11 libxft libxcb lua5.4 rav1e rtmpdump rubberband sdl2 libsnappy libspeex libsrt tesseract libtheora libwebp x264 x265 libxvidcore xz  $(VLC_EXTRA_DEPS) vlc-setup
 	cd $(BUILD_WORK)/vlc && ./bootstrap && LDFLAGS="-framework CoreFoundation -framework CFNetwork" ./configure -C \
 		$(DEFAULT_CONFIGURE_FLAGS) \
 		--disable-qt \
@@ -56,7 +56,7 @@ vlc: aom dav1d ffmpeg fontconfig freetype frei0r gnutls lame libarchive libass l
 		--enable-dvdread \
 		--enable-static \
 		--enable-shared \
-		--enable-sftp \
+		--disable-sftp \
 		--enable-run-as-root \
 		--enable-x264 \
 		--enable-x265 \
@@ -88,6 +88,8 @@ vlc: aom dav1d ffmpeg fontconfig freetype frei0r gnutls lame libarchive libass l
 	+$(MAKE) -C $(BUILD_WORK)/vlc
 	+$(MAKE) -C $(BUILD_WORK)/vlc install \
 		DESTDIR=$(BUILD_STAGE)/vlc
+	+$(MAKE) -C $(BUILD_WORK)/vlc install \
+                DESTDIR=$(BUILD_BASE)
 	touch $(BUILD_WORK)/vlc/.build_complete
 endif
 
